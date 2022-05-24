450df85fa40a566c8f2d5fe98b025d324eee3319
Pedro <thanksuniverse333@outlook.com>
2022-05-24 18:19:58 -0300

    Before Homework

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 	let titulos = $(" h4 "); //*tag
 
-	let items = $(".featired-item"); //*class
+	let items = $(".featured-item"); //*class
 
 	let destaques = $("#featured"); //*id
 
@@ -29,19 +29,47 @@ $(document).ready(function () {
 	//Hide adiciona display none em um elemento
 	//Show readiciona o display no elemento
 	//Fade adiciona uma transição ao elemento .fadeIn(2000) or .fadeOut(2000)
-
+	/* let fi = $(".featured-item"); //Start loading CSS
+	fi.css({
+		"text-align": "center",
+		"background-color": "wheat",
+	}); */
 	$(".featured-item img").fadeOut(1);
 	$(".featured-item img").fadeIn(1000);
-	$(".featured-item:first h4").css("color", "#f00");
-	$(".featured-item:first h4").css({
-		color: "#f00",
-		background: "#ff0",
-		border: "1px solid #fff",
-		"font-weight": "100",
-	});
+	/* $(".featured-item:first h4").css("color", "#f00"); */
+	/* $(".featured-item").dblclick(function () {
+		//dblclick - Double Click
+		//OnClick CSS
+		$(this).css({
+			color: "#f00",
+			background: "#ff0",
+			border: "1px solid #fff",
+			"font-weight": "100",
+		});
+	}); */
+
 	//É possível colocar vários elementos de estilo de css pelo JavaScript com o código $('classe, id, name').css({
 
 	//})
+	$(".featured-item").hover(
+		function () {
+			//mouseenter
+			console.log($(this).find("h4").text());
+		},
+		function () {
+			//mouseleave
+			console.log($(this).find("h4").text() + " - " + "Preço:" + " - " + $(this).find("h6").text());
+		}
+	);
+
+	//*Manipulação de evento
+
+	$(".featured-item a").on("click", function (event) {
+		//on('click', 'mouseenter', 'mouseleave', 'blur') /Blur: Quando tira o foco- Clicar no botão e depois clica fora -- Utilizado em campos de formulários
+		event.preventDefault();
+
+		alert("Produto esgotado");
+	});
 });
 
 /* for (let i = 0; i < titulos.length; i++) {
